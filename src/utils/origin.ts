f049cd3ef403aa7e82148d5f0e8d14ab170aa5da
Leonardo <accs.lndrs@gmail.com>
2024-04-11 21:51:00 -0300

    chore: improved origin checker

--- src/utils/origin.ts
+++ src/utils/origin.ts
@@ -1,3 +1,11 @@
+const translateReferrer = (referrer: string): string => {
+  if (referrer.includes("t.co")) {
+    return "https://x.com";
+  }
+
+  return referrer;
+};
+
 export function getOrigin(query: string): string {
   const origin = query.split("&").find((q) => q.startsWith("o="));
 
@@ -5,6 +13,6 @@ export function getOrigin(query: string): string {
     case "x":
       return "https://x.com";
     default:
-      return document.referrer;
+      return translateReferrer(document.referrer);
   }
 }
